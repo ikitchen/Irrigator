5c499ed4c96917e6ef1f197f22d6e2140418b7f7
no <no>
2018-09-06 15:47:23 +0300

    binary response

--- server/mainService.js
+++ server/mainService.js
@@ -1,5 +1,25 @@
 const { getForecast } = require('./weatherClient');
 
+const FLAG_RAINY = 0x01;
+
+const encode = ({ isRainy, currentTime }) => {
+    const buff = new ArrayBuffer(
+        Uint8Array.BYTES_PER_ELEMENT + Uint32Array.BYTES_PER_ELEMENT
+    );
+
+    const view = new DataView(buff);
+
+    const flags = 0;
+    if (isRainy) {
+        flags |= FLAG_RAINY;
+    }
+
+    view.setUint8(0, flags);
+    view.setUint32(1, currentTime);
+
+    return new Buffer(buff);
+};
+
 const isItemRainy = singleForecast =>
     singleForecast.weather.some(weather => weather.main === 'rain');
 
@@ -8,8 +28,10 @@ const mainService = async (ctx, next) => {
     const forecastsList = forecast.list;
     const isRainy =
         isItemRainy(forecastsList[0]) || isItemRainy(forecastsList[1]);
-    const curentTime = Date.now();
-    ctx.body = `${isRainy ? 1 : 0}:${curentTime}`;
+    const currentTime = Math.round(Date.now() / 1000);
+
+    // ctx.set('Content-Type', 'text/html');
+    ctx.body = encode({ currentTime, isRainy });
 };
 
 module.exports = mainService;
